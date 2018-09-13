6a62b86bf0141016a878f1fc941c68b919d2e29a
yangyize <yangyize@gmail.com>
2018-09-13 19:08:16 +0800

    add file tools

--- src/sshx.c
+++ src/sshx.c
@@ -7,24 +7,54 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 
+enum use_code {
+    LIST_REC,
+    USE_ID,
+    STORE_ACCESS,
+    IGNORE_ACCESS,
+};
+
+struct {
+    int id;
+    char *name;
+    char *user;
+    char *host;
+    int port;
+    char *password;
+    int type;
+} user_obj;
+
 void usage(int status);
 
 char *opt_str(char *optarg, char *argv_optind);
 
-void child(char *user, char *address, long port, char *name, char *password);
+void child();
 
 char *makestr(char *buf);
 
 void sigchild_handler(int signum);
 
 void winch_handler(int signum);
-git
+
 int handleoutput(int fd);
+
+void read_index();
+
+void list_rec();
+
+void store_access();
+
+long fsize(FILE *file);
+
+void fcopy(FILE *fdst, long d, FILE *fsrc, long s, long offset);
+
+char *fname(FILE *file);
 
 struct option long_options[] = {
         {"address",  required_argument, NULL, 'a'},
         {"help",     no_argument,       NULL, 'h'},
         {"id",       required_argument, NULL, 'i'},
+        {"list",     no_argument,       NULL, 'l'},
         {"name",     required_argument, NULL, 'n'},
         {"password", required_argument, NULL, 'P'},
         {"port",     required_argument, NULL, 'p'},
@@ -35,42 +65,52 @@ struct option long_options[] = {
 static int master_pt_fd;
 static int tty_fd;
 static char *pt_name;
-char password[1024] = {'\0'};
 
 int main(int argc, char *argv[]) {
     int newpid;
     int optc;
     struct winsize wbuf;
 
-    char user[1024] = "root";
-    char address[1024] = {'\0'};
-    char name[1024] = {'\0'};
-    long port = 22;
-    char *port_p;
+    user_obj.user = "root";
+    user_obj.port = 22;
+    user_obj.type = STORE_ACCESS;
 
     char *tmp;
     int optionindex = -1;
 
-
     if (argc <= 1) {
         usage(1);
     } else {
-        while ((optc = getopt_long(argc, argv, "a:hi:n:P:p:u:", long_options, &optionindex)) != -1) {
+        while ((optc = getopt_long(argc, argv, "a:hi:ln:P:p:u:", long_options, &optionindex)) != -1) {
             printf("opt = %c\t\t", optc);
             printf("optarg = %s\t\t", optarg);
             printf("optind = %d\t\t", optind);
             printf("argv[optind] = %s\n", argv[optind]);
+            char *port_p;
+            char *id_str;
             switch (optc) {
                 case 0:
                     break;
                 case 'a':
-                    strcpy(address, opt_str(optarg, argv[optind]));
+                    strcpy(user_obj.host, opt_str(optarg, argv[optind]));
+                    break;
+                case 'i':
+                    id_str = opt_str(optarg, argv[optind]);
+                    if (id_str) {
+                        long id = strtol(id_str, &tmp, 10);
+                        if (id > 65535) {
+                            fprintf(stderr, "Error: id is too big\n");
+                        }
+                        user_obj.id = (int) id;
+                        user_obj.type = USE_ID;
+                    }
                     break;
                 case 'n':
-                    strcpy(name, opt_str(optarg, argv[optind]));
+                    strcpy(user_obj.name, opt_str(optarg, argv[optind]));
                     break;
                 case 'p':
                     port_p = opt_str(optarg, argv[optind]);
+                    long port = 22;
                     if (port_p != NULL) {
                         port = strtol(port_p, &tmp, 10);
                     }
@@ -78,12 +118,16 @@ int main(int argc, char *argv[]) {
                         fprintf(stderr, "Error: port is too big\n");
                         exit(1);
                     }
+                    user_obj.port = (int) port;
+                    break;
+                case 'l':
+                    user_obj.type = LIST_REC;
                     break;
                 case 'P':
-                    strcpy(password, opt_str(optarg, argv[optind]));
+                    strcpy(user_obj.password, opt_str(optarg, argv[optind]));
                     break;
                 case 'u':
-                    strcpy(user, opt_str(optarg, argv[optind]));
+                    strcpy(user_obj.user, opt_str(optarg, argv[optind]));
                 case 'h':
                     usage(0);
                     return 0;
@@ -115,15 +159,31 @@ int main(int argc, char *argv[]) {
 
         pt_name = ttyname(master_pt_fd);
 
+        int slave_pt_fd;
+
+        if (user_obj.type == LIST_REC) {
+            list_rec();
+            return 0;
+        } else if (user_obj.type == USE_ID) {
+            read_index();
+        } else if (user_obj.type == STORE_ACCESS) {
+            if (!user_obj.name) {
+                user_obj.name = user_obj.host;
+            }
+            store_access();
+        }
+
+
         if ((newpid = fork()) == -1) {
             perror("fork");
         } else if (newpid == 0) {
-            child(user, address, port, name, password);
+            child();
         } else if (newpid < 0) {
             perror("fail ");
         }
 
-        int slave_pt_fd = open(pt_name, O_RDWR | O_NOCTTY);
+
+        slave_pt_fd = open(pt_name, O_RDWR | O_NOCTTY);
 
         int status = 0;
         int terminate = 0;
@@ -142,8 +202,8 @@ int main(int argc, char *argv[]) {
                 fd_set readfd;
                 FD_ZERO(&readfd);
                 FD_SET(master_pt_fd, &readfd);
-
                 printf("try to pselect\n");
+
                 int selret = pselect(master_pt_fd + 1, &readfd, NULL, NULL, NULL, &sigmask_select);
                 printf("pselect %d\n", selret);
                 if (selret > 0) {
@@ -163,6 +223,7 @@ int main(int argc, char *argv[]) {
                         }
                     }
                 }
+
                 wait_id = waitpid(newpid, &status, WNOHANG);
             } else {
                 wait_id = waitpid(newpid, &status, 0);
@@ -173,8 +234,8 @@ int main(int argc, char *argv[]) {
 }
 
 
-void child(char *user, char *address, long port, char *name, char *password) {
-//    setsid();
+void child() {
+    setsid();
     int sla_fd;
     char *arglist[1024];
     int numargs = 0;
@@ -187,15 +248,15 @@ void child(char *user, char *address, long port, char *name, char *password) {
     close(master_pt_fd);
     arglist[numargs++] = ssh;
 
-    strcpy(temp1, user);
+    strcpy(temp1, user_obj.user);
     strcat(temp1, "@");
-    strcat(temp1, address);
+    strcat(temp1, user_obj.host);
     arglist[numargs++] = makestr(temp1);
 
     memset(temp1, 0, 2049);
 
     memset(temp1, 0, 2049);
-    sprintf(temp2, "%li", port);
+    sprintf(temp2, "%d", user_obj.port);
     strcat(temp1, "-p");
     strcat(temp1, temp2);
     arglist[numargs++] = makestr(temp1);
@@ -250,6 +311,110 @@ void usage(int status) {
     }
 }
 
+void list_rec() {
+    FILE *file = fopen("ssh", "r+");
+    if (file == NULL) {
+        fprintf(stderr, "read ssh.txt fail");
+    }
+    char line[1024];
+
+    printf("%s\t%s\t%s\t%s\t%s\t%s\n",
+           "id",
+           "name",
+           "user",
+           "host",
+           "port",
+           "password");
+    int id = 1;
+    while (!feof(file)) {
+        fgets(line, 1024, file);
+        printf("%d\t%s\n", id++, line);
+    }
+    fclose(file);
+}
+
+void read_index() {
+    if (!user_obj.id) {
+        fprintf(stderr, "Error: Unknown id\n");
+    }
+
+    FILE *file = fopen("ssh.txt", "r+");
+    if (file == NULL) {
+        fprintf(stderr, "read ssh.txt fail");
+    }
+
+    int r_id = 1;
+    char *r_name = NULL;
+    char *r_user = NULL;
+    char *r_host = NULL;
+    int r_port;
+    char *r_password = NULL;
+
+    while (!feof(file)) {
+        fscanf(file, "%s\t%s\t%s\t%d\t%s\n",
+               r_name,
+               r_user,
+               r_host,
+               &r_port,
+               r_password
+        );
+        if (r_id++ == user_obj.id) {
+            user_obj.name = r_name;
+            user_obj.user = r_user;
+            user_obj.host = r_host;
+            user_obj.port = r_port;
+            user_obj.password = r_password;
+            fclose(file);
+            return;
+        }
+    }
+
+    fclose(file);
+}
+
+void store_access() {
+    FILE *file = fopen("ssh.txt", "r+");
+    if (file == NULL) {
+        fprintf(stderr, "read ssh.txt fail");
+    }
+
+    char *r_name = NULL;
+    char *r_user = NULL;
+    char *r_host = NULL;
+    int r_port;
+    char *r_password = NULL;
+
+    while (!feof(file)) {
+        fscanf(file, "%s\t%s\t%s\t%d\t%s\n",
+               r_name,
+               r_user,
+               r_host,
+               &r_port,
+               r_password
+        );
+        if (user_obj.host == r_host && user_obj.port == r_port) {
+            if (user_obj.name != r_name) {
+
+            }
+
+            if (user_obj.password != r_password) {
+
+            }
+            fclose(file);
+            return;
+        }
+    }
+
+    fprintf(file, "%s\t%s\t%s\t%d\t%s\n",
+            user_obj.name,
+            user_obj.user,
+            user_obj.host,
+            user_obj.port,
+            user_obj.password);
+    fclose(file);
+}
+
+
 int handleoutput(int fd) {
     static int prevmatch = 0;
 
@@ -260,7 +425,7 @@ int handleoutput(int fd) {
     buffer[numread] = '\0';
 
     if (!prevmatch) {
-        write(fd, password, strlen(password));
+        write(fd, user_obj.password, strlen(user_obj.password));
         write(fd, "\n", 1);
         prevmatch = 1;
     } else {
@@ -270,6 +435,57 @@ int handleoutput(int fd) {
 
     return ret;
 }
+
+int fdel(FILE *file, long offset, int len) {
+    char *file_name;
+    long file_size = fsize(file);
+    FILE *temp;
+    if (offset > file_size || offset < 0 || len < 0) {
+        return -1;
+    }
+    temp = tmpfile();
+    fcopy(temp, 0, file, 0, offset);
+    fcopy(temp, offset, file, offset + len, -1);
+    file_name = fname(file);
+    freopen(file_name, "wb+", file);
+    fcopy(file, 0, temp, 0, -1);
+    fclose(temp);
+    return 0;
+}
+
+char *fname(FILE *file) {
+    char fd_path[1024] = {'\0'};
+    char *filename = malloc(1024);
+    int fd = fileno(file);
+    sprintf(fd_path, "/proc/self/fd/%d", fd);
+    ssize_t r = readlink(fd_path, filename, 1023);
+    if (r < 0) {
+        // TODO error
+        return NULL;
+    }
+    filename[r] = '\0';
+    return filename;
+}
+
+void fcopy(FILE *fdst, long d, FILE *fsrc, long s, long offset) {
+    fseek(fdst, d, SEEK_CUR);
+    fseek(fsrc, s, SEEK_CUR);
+
+    int c;
+    int i = 0;
+    while ((c = fgetc(fsrc)) != EOF && (i < offset || i < 0)) {
+        fputc(c, fdst);
+    }
+}
+
+long fsize(FILE *file) {
+    if (!file) {
+        return -1;
+    }
+    fseek(file, 0L, SEEK_END);
+    return ftell(file);
+}
+
 
 void sigchild_handler(int signum) {
 
@@ -281,3 +497,4 @@ void winch_handler(int signum) {
         ioctl(master_pt_fd, TIOCGWINSZ, &wsize);
     }
 }
+
